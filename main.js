9ccccc1662c2f71eb35c612e95a26a3bc32ddc7d
nbonato <akatux@gmail.com>
2023-01-11 20:25:58 +0100

    Update with latest changes

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const addTaskForm = document.getElementById("new-task");
 
 let tasks = [];
 
-function task(title) {
+function task(title, dueDate, type) {
     this.title = title;
     this.delete = function() {
         tasks.splice(tasks.indexOf(this), 1);
         updateTaskList();
     };
+    this.dueDate = dueDate;
+    this.type = type;
 };
 
 // this clears and re-generates the whole list based on the array "tasks"
@@ -16,30 +18,59 @@ function updateTaskList () {
     taskList.innerHTML = "";
     for (let task of tasks) {
         let newTask = document.createElement("li");
+        let taskBasic = document.createElement("div");
+        taskBasic.className = "task-basic";
         let taskName = document.createElement("p");
         let deleteButton = document.createElement("p");
+        taskBasic.appendChild(taskName);
+        taskBasic.appendChild(deleteButton);
+        
+        let taskInfo = document.createElement("div");
+        let dueDateElement = document.createElement("p");
+        dueDateElement.textContent = task.dueDate;
+        taskInfo.appendChild(dueDateElement);
+        let typeElement = document.createElement("p");
+        typeElement.textContent = task.type;
+        taskInfo.appendChild(typeElement);
+        
+        taskInfo.classList.add("hidden");
+        taskInfo.classList.add("task-info");
+        
         deleteButton.textContent = "✖"
         deleteButton.classList.add("delete-button");
         deleteButton.addEventListener("click", (event) => {
             task.delete();
         });
         taskName.textContent = task.title;
-        newTask.appendChild(taskName);
-        newTask.appendChild(deleteButton);
+        newTask.appendChild(taskBasic);
+        newTask.appendChild(taskInfo);
+        newTask.addEventListener("click", () => {
+            for (let element of document.querySelectorAll(".task-info")) {
+                if (!element.classList.contains("hidden")) {
+                    element.classList.toggle("hidden");
+                };
+            }; 
+            taskInfo.classList.toggle("hidden");
+        });
         taskList.appendChild(newTask);
     };
 };
 
 addTaskForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    let input = addTaskForm.querySelector("input[type='text']");
+    let title = document.getElementById("task-input").value;
+    let dueDate = document.getElementById("date-input").value; 
+    let type = document.getElementById("type-choice").value; 
+
     // check that the input isn't empty before adding a task
-    if (input != "") { 
-        // use the text in the input as the title of the task
-        let newTask = new task(input.value); 
+    if (title != "") { 
+        // use the input as the task values
+        let newTask = new task(title, dueDate, type);
         // add the task to the tasks list
         tasks.push(newTask)
         updateTaskList();
+    } else {
+        alert("No name!");
     };
     // reset the form so that it's empty
     addTaskForm.reset();
